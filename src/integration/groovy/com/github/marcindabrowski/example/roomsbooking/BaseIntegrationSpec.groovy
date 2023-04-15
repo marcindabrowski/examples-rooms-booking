b750eb13dc7c1127d3fcc30a5f56c0d7ad100174
Marcin Dąbrowski <3007876+marcindabrowski@users.noreply.github.com>
2023-04-15 17:19:38 +0200

    feat: update spring boot to v3 (#99)

--- src/integration/groovy/com/github/marcindabrowski/example/roomsbooking/BaseIntegrationSpec.groovy
+++ src/integration/groovy/com/github/marcindabrowski/example/roomsbooking/BaseIntegrationSpec.groovy
@@ -3,8 +3,10 @@ package com.github.marcindabrowski.example.roomsbooking
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
 @SpringBootTest(classes = [RoomsBookingApplication],
         properties = "application.environment=integration",
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-abstract class BaseIntegrationTest extends Specification {
+        webEnvironment = RANDOM_PORT)
+abstract class BaseIntegrationSpec extends Specification {
 }
--- src/main/java/com/github/marcindabrowski/example/roomsbooking/config/BookingServiceConfiguration.java
+++ src/main/java/com/github/marcindabrowski/example/roomsbooking/config/BookingServiceConfiguration.java
@@ -2,8 +2,6 @@ package com.github.marcindabrowski.example.roomsbooking.config;
 
 import java.math.BigDecimal;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties(prefix = "booking-service")
-@ConstructorBinding
 public record BookingServiceConfiguration(BigDecimal premiumRoomMinimumPrice) {}
