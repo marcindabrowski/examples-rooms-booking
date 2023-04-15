136108d12a4776c598836066455cdf269595a66a
Marcin Dąbrowski <3007876+marcindabrowski@users.noreply.github.com>
2023-04-15 16:29:28 +0200

    chore: switch from AOSP to Default Google Java Style Format (#97)

--- src/main/java/com/github/marcindabrowski/example/roomsbooking/RoomsBookingApplication.java
+++ src/main/java/com/github/marcindabrowski/example/roomsbooking/RoomsBookingApplication.java
@@ -1,7 +1,6 @@
 package com.github.marcindabrowski.example.roomsbooking;
 
 import com.github.marcindabrowski.example.roomsbooking.config.BookingServiceConfiguration;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,7 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 public class RoomsBookingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RoomsBookingApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(RoomsBookingApplication.class, args);
+  }
 }
